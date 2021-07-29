a3f38edd7e57edbfbfdb7687572c2bd44636affd
Itiel Olenick <etl676@gmail.com>
2021-07-29 13:58:24 +0300

    cleaned the whole project

--- src/main/java/com/exercise/fitnessapp/controller/WorkoutController.java
+++ src/main/java/com/exercise/fitnessapp/controller/WorkoutController.java
@@ -2,6 +2,7 @@ package com.exercise.fitnessapp.controller;
 
 import com.exercise.fitnessapp.entity.Workout;
 import com.exercise.fitnessapp.repository.WorkoutRepository;
+import org.hibernate.jdbc.Work;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,5 +26,10 @@ public class WorkoutController {
     @PostMapping("/workouts")
     public ResponseEntity<Workout> creatWorkout(@RequestBody Workout workout){
         return new ResponseEntity<Workout>(workoutRepository.save(workout), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/workouts/{id}")
+    public ResponseEntity<Workout> readWorkout (@PathVariable Integer id) {
+        return new ResponseEntity<Workout>(workoutRepository.findById(id).get(), HttpStatus.OK);
     }
 }
